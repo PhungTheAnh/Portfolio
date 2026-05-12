1ac6837dcd084b447a966810d6010ee0007b9b6b
Phung The Anh <phungtheanh2001@gmail.com>
2026-05-12 17:38:11 +0700

    update

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -12,27 +12,68 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-// smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+var lenis = new Lenis({
+    duration: 0.6,
+    wheelMultiplier: 1,
+    touchMultiplier: 1.5,
+    infinite: false
+});
 
-        if (this.hash !== "") {
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 
-            event.preventDefault();
+$(document).ready(function () {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        var hash = $(this).attr('href');
+        if (hash && hash.length > 1) {
+            var target = document.querySelector(hash);
+            if (target) {
+                event.preventDefault();
+                lenis.scrollTo(target, { offset: -60 });
 
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
+                if ($(window).width() < 992) {
+                    $('#nav-toggle').removeClass('is-active');
+                    $('ul.nav').removeClass('show');
+                }
+            }
+        }
     });
+
+    $('#nav-toggle').click(function () {
+        $(this).toggleClass('is-active');
+        $('ul.nav').toggleClass('show');
+    });
+
+    initScrollReveal();
 });
 
-// navbar toggle
-$('#nav-toggle').click(function(){
-    $(this).toggleClass('is-active')
-    $('ul.nav').toggleClass('show');
-});
+function initScrollReveal() {
+    var sections = document.querySelectorAll('.section, .footer');
+    sections.forEach(function (section) {
+        var children = section.querySelectorAll(
+            '.section-subtitle, .section-title, .about, .skill-card, .timeline-item, .contact-form, .form-row'
+        );
+        children.forEach(function (child, index) {
+            child.classList.add('reveal');
+            if (index < 5) {
+                child.classList.add('reveal-delay-' + (index + 1));
+            }
+        });
+    });
+
+    var observer = new IntersectionObserver(function (entries) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('revealed');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    document.querySelectorAll('.reveal').forEach(function (el) {
+        observer.observe(el);
+    });
+}
